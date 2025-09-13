c50f5c837df78d0ae00c64ea9cee57c2048539c7
Chuck Testa <112298806+ChuckTestaaa@users.noreply.github.com>
2025-09-13 16:34:45 +0800

    Add an event listener to the container If the target contains the 'box' class then change the background of the target to red

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ let container = document.querySelector('.container');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const newDiv = document.createElement('div');
-        newDiv.textContent = `Box: ${j}`;
+        newDiv.classList.add('box')
+
         container.appendChild(newDiv);
     }
 
 }
 container.style.backgroundColor = 'antiquewhite';
+
+container.addEventListener('mouseover', (event) => {
+    if (event.target.classList.contains('box')) {
+        event.target.style.backgroundColor = 'red';
+    }
+
+});
+
+
