a90c516dde4217b0216e2ed506e5e071b55660d8
Chuck Testa <112298806+ChuckTestaaa@users.noreply.github.com>
2025-09-14 15:54:26 +0800

    Add a click event listener to button with resetGrid callback that gets a prompt for new size and clears the container and calls gridSize

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 let container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('box')
+gridSize(16);
 
-        container.appendChild(newDiv);
+function gridSize(size) {
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('box')
+            newDiv.textContent = `BOX: ${j}`
+            container.appendChild(newDiv);
+        }
+
     }
-
 }
+
+
 container.style.backgroundColor = 'antiquewhite';
 
 container.addEventListener('mouseover', (event) => {
     if (event.target.classList.contains('box')) {
-        event.target.style.backgroundColor = 'red';
+        event.target.style.backgroundColor = 'gray';
     }
 
 });
+
+let reset = document.querySelector('#reset');
+
+
+reset.addEventListener('click', resetGrid);
+
+function resetGrid() {
+    let newSize = prompt("Enter grid size");
+    container.replaceChildren();
+    gridSize(newSize);
+
+}
 
 
